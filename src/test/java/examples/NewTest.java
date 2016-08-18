9d81b91c7b079a76d7a8cc3027d93604313bc95b
nvupkoor <nvupkoor@cisco.com>
2016-08-18 06:58:28 +0530

    first commit

---
+++ src/test/java/examples/NewTest.java
@@ -0,0 +1,29 @@
+package examples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	private	WebDriver driver; 
+	
+	@BeforeTest
+	  public void beforeTest() {
+		driver = new FirefoxDriver();
+	  }
+  @Test
+  public void f() {
+	  driver.get("http://www.guru99.com/selenium-tutorial.html");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Free Selenium Tutorials")); 
+  }
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();	
+  }
+
+}
